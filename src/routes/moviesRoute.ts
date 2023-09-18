587a0c688a0cef92a887d916518b613df92b1c86
anapacker <anaamaria.packer@gmail.com>
2023-09-18 12:14:02 -0300

    feat: validação joi de movies

--- src/routes/moviesRoute.ts
+++ src/routes/moviesRoute.ts
@@ -1,7 +1,10 @@
+import { validateSchemas } from "middleware/validateSchemas";
 import { moviesController } from "../controllers/moviesController";
 import { Router } from "express";
+import movieSchema from "schemas/movieSchema";
 
 const moviesRoute = Router()
-moviesRoute.post("/movies", moviesController.moviesPost)
+moviesRoute.post("/movies",validateSchemas(movieSchema), moviesController.moviesPost)
+moviesRoute.get("/movies")
 
 export default moviesRoute
---
+++ src/schemas/movieSchema.ts
@@ -0,0 +1,12 @@
+import Joi from "joi"
+import { Movie } from "protocols"
+
+const movieSchema = Joi.object<Movie>({
+  name: Joi.string().max(99).min(3).required(),
+  ano:Joi.string().min(4).required(),
+  plataformaId:Joi.number().required(),
+  genero:Joi.string().required(),
+  assistido:Joi.boolean()
+
+})
+export default movieSchema
